d5721f3a294faba71d4e3efe286dbf011ba9f2a9
Danil <Akhmatdinov.dr@fixline.tech>
2022-10-21 23:17:41 +0500

    Доработано автополучение имени проекта и пути винсс из конифга

--- src/extension.ts
+++ src/extension.ts
@@ -7,122 +7,154 @@ import * as fs from 'fs';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	let disposable1 = vscode.commands.registerCommand('extension.OpenPanel', OpenPanel);
-    let disposable2 = vscode.commands.registerCommand('extension.CheckScript', CheckScript);
-    let disposable3 = vscode.commands.registerCommand('extension.OpenLogs', OpenLog);
+	let disposable2 = vscode.commands.registerCommand('extension.CheckScript', CheckScript);
+	let disposable3 = vscode.commands.registerCommand('extension.OpenLogs', OpenLog);
 	context.subscriptions.push(disposable1);
 	context.subscriptions.push(disposable2);
 	context.subscriptions.push(disposable3);
 }
 function OpenPanel(uri: vscode.Uri) {
-    let path = uri?.fsPath;
-    if(path == undefined) path = vscode.window.activeTextEditor?.document.uri.fsPath!;
-    if(path == undefined || path == '') return;
+	let path = uri?.fsPath;
+	if(path == undefined) path = vscode.window.activeTextEditor?.document.uri.fsPath!;
+	if(path == undefined || path == '') return;
 	if(path.indexOf('\\panels\\') !== -1) {
 		path = path.slice(path.indexOf("panels") + 7);
 		let conf = getCommands(getConfiguration());
-		let command = conf[0].pathWinCC +'/bin/WCCOAui.exe -p ' + path + ' -user ' + conf[0].user + ':' + conf[0].pass + ' -proj ' + conf[0].projName;
-        const output = execShell(command);
+		let command = getPathInConfigFile('pvss_path') +'/bin/WCCOAui.exe -p ' + path + ' -user ' + conf[0].user + ':' + conf[0].pass + ' -proj ' + getPathInConfigFile('proj_name');
+		const output = execShell(command);
 
 	}
 
 }
 function CheckScript(uri: vscode.Uri) {
-    let path = uri?.fsPath;
-    if(path == undefined) path = vscode.window.activeTextEditor?.document.uri.fsPath!;
-    if(path == undefined || path == '') return;
+	let path = uri?.fsPath;
+	if(path == undefined) path = vscode.window.activeTextEditor?.document.uri.fsPath!;
+	if(path == undefined || path == '') return;
 	if(path.indexOf('\\scripts\\') !== -1) {
 		path = path.slice(path.indexOf("scripts") + 8);
 		let conf = getCommands(getConfiguration());
-		let command = conf[0].pathWinCC +'/bin/\WCCOActrl.exe -syntax -proj ' + conf[0].projName + ' ' + path;
-        const output = execShell(command);
+		let command = getPathInConfigFile('pvss_path') +'/bin/\WCCOActrl.exe -syntax -proj ' + getPathInConfigFile('proj_name') + ' ' + path;
+		console.log(command);
+		const output = execShell(command);
 	}
 
 }
 const execShell = (cmd: string) =>
-    new Promise<string>((resolve, reject) => {
-      cp.exec(cmd, (err, out) => {
-        if (err) {
-          return resolve('error!');
-          //or,  reject(err);
-        }
-        return resolve(out.toString());
-      });
-    });
+	new Promise<string>((resolve, reject) => {
+	  cp.exec(cmd, (err, out) => {
+		if (err) {
+		  return resolve('error!');
+		  //or,  reject(err);
+		}
+		return resolve(out.toString());
+	  });
+	});
 function getConfiguration() {
-    return vscode.workspace
-        .getConfiguration()
-        .get('OpenPanel.commands');
+	return vscode.workspace
+		.getConfiguration()
+		.get('OpenPanel.commands');
 }
 function getCommands(configuration: any) {
-    if (!Array.isArray(configuration)) {
-        return [];
-    }
-    return configuration
-        .map((c) => {
-        const maybeCommand = c;
-        return {
-            projName: maybeCommand.projName,
-            pathWinCC: maybeCommand.pathWinCC,
-            user: maybeCommand.user,
-            pass: maybeCommand.pass
-        };
-    });
+	if (!Array.isArray(configuration)) {
+		return [];
+	}
+	return configuration
+		.map((c) => {
+		const maybeCommand = c;
+		return {
+			// projName: maybeCommand.projName,
+			// pathWinCC: maybeCommand.pathWinCC,
+			user: maybeCommand.user,
+			pass: maybeCommand.pass
+		};
+	});
 }
 let doLogs = true;
 async function OpenLog() {
-    let winccLog_channel = vscode.window.createOutputChannel("WinccLogs");
-    winccLog_channel.show(true);
-    let path = 'D:/Projects/WinCC_OA_Proj/Configurator/log/';
-    let fileNames = getFileNames(path);
-    interface MyType {
-        [key: string]: number;
-    }
-    let fileSizes: MyType = {};
-    fileNames.forEach(element => {
-        fileSizes[element] = fs.statSync(path + element).size;
-    })
-    while(doLogs) {
-        fileNames.forEach(element => {
-            let newSizeFile = fs.statSync(path + element).size;
-            if(newSizeFile > fileSizes[element])
-            {
-                let buffer = Buffer.alloc(newSizeFile - fileSizes[element]);
-                fs.open(path + element, 'r+', function (err, fd) {
-                    if (err) {
-                        return console.error(err);
-                    }
-                    fs.read(fd, buffer, 0, buffer.length,
-                        fileSizes[element], function (err, bytes) {
-                            if (err) {
-                                console.log(err);
-                            }
-                            if (bytes > 0) {
-                                winccLog_channel.append(buffer.
-                                    slice(0, bytes).toString());
-                            }
-                            fs.close(fd, function (err) {
-                                if (err) {
-                                    console.log(err);
-                                }
-                            });
-                        });
-                    fileSizes[element] = newSizeFile;
-                });
-            }
-        })
-        await delay(1000);
-    }
+	let path = getPathInConfigFile('proj_path') + '/log/';
+	if(path == '/log/' || !fs.existsSync(path)) {
+		vscode.window.showErrorMessage('Не найден файл конфигурации');
+		return;
+	} 
+	let winccLog_channel = vscode.window.createOutputChannel("WinccLogs");
+	winccLog_channel.show(true);
+	let fileNames = getFileNames(path);
+	interface MyType {
+		[key: string]: number;
+	}
+	let fileSizes: MyType = {};
+	fileNames.forEach(element => {
+		fileSizes[element] = fs.statSync(path + element).size;
+	})
+	while(doLogs) {
+		fileNames.forEach(element => {
+			let newSizeFile = fs.statSync(path + element).size;
+			if(newSizeFile > fileSizes[element])
+			{
+				let buffer = Buffer.alloc(newSizeFile - fileSizes[element]);
+				fs.open(path + element, 'r+', function (err, fd) {
+					if (err) {
+						return console.error(err);
+					}
+					fs.read(fd, buffer, 0, buffer.length,
+						fileSizes[element], function (err, bytes) {
+							if (err) {
+								console.log(err);
+							}
+							if (bytes > 0) {
+								winccLog_channel.append(buffer.
+									slice(0, bytes).toString());
+							}
+							fs.close(fd, function (err) {
+								if (err) {
+									console.log(err);
+								}
+							});
+						});
+					fileSizes[element] = newSizeFile;
+				});
+			}
+		})
+		await delay(1000);
+	}
 }
 
 function getFileNames(path: string) : string[]{
-    let fileNames = fs.readdirSync(path).filter(element => {
-        return element.indexOf('.log') > 0;
-    })
-    return fileNames;
+	let fileNames = fs.readdirSync(path).filter(element => {
+		return element.indexOf('.log') > 0;
+	})
+	return fileNames;
 }
 function delay(ms: number) {
-    return new Promise( resolve => setTimeout(resolve, ms) );
+	return new Promise( resolve => setTimeout(resolve, ms) );
 }
-
+function getPathInConfigFile(what: string): string {
+	let path = '';
+	let regexp: RegExp;
+	if(what == 'proj_path') {
+		regexp = /^proj_path = \"(.*)\"/;
+	}
+	else if(what == 'pvss_path') {
+		regexp = /^pvss_path = \"(.*)\"/;
+	}
+	else if(what == 'proj_name') {
+		regexp = /^proj_path = .*[\/ | \\](.*)"/;
+	}
+	else return '';
+	vscode.workspace.workspaceFolders?.find(wsFolder => {
+		if (fs.existsSync(wsFolder.uri.fsPath + '/config/config')) {
+			let fileData = fs.readFileSync(wsFolder.uri.fsPath + '/config/config', 'utf8');
+			let linesData = fileData.split('\n');
+			linesData.forEach(line =>{
+				const matches = line.match(regexp) || [];
+				if(matches.length > 0)
+				{
+					path = matches[1];
+				}
+			});
+		}
+	});
+	return path;
+}
 // this method is called when your extension is deactivated
 export function deactivate() {}
