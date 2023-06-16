d2474b32b9a4f33297e0620894fce4c775be40e9
Danil <Akhmatdinov.dr@fixline.tech>
2023-06-16 20:02:04 +0500

    добавлен юсес

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,13 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
 import { CtrlSymbolsCreator } from './ctrlSymbolsCreator';
 import { panelPreviewProvider } from './panelPreviewProvider';
 import { CtrlCompletionItemProvider} from './ctrlProvideCompletionItems';
 import { CtrlGoDefinitionProvider } from './CtrlGoDefinitionProvider';
 import * as cmdCtrl from './ctrlComands';
+import * as fs from 'fs';
+import * as path from 'path';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -44,6 +45,43 @@ export function activate(context: vscode.ExtensionContext) {
 	  )
 	);
 
+	const providerUses = vscode.languages.registerCompletionItemProvider(
+		'ctrlpp',
+		{
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+				const linePrefix = document.lineAt(position).text.substr(0, position.character);
+				const symbol = new vscode.CompletionItem('uses ', vscode.CompletionItemKind.Keyword);
+				return [symbol];
+			}
+		},
+		'#' // triggered whenever a '.' is being typed
+	);
+	const providerFiles = vscode.languages.registerCompletionItemProvider(
+		'ctrlpp',
+		{
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+				const linePrefix = document.lineAt(position).text.substr(0, position.character);
+				if(linePrefix.startsWith('#uses')) {
+					let folder = GetProjectsInConfigFile()[0] + '/scripts/libs';
+					let files: string[] = new Array;
+					let complets = new Array;
+					ThroughDirectory(folder, files);
+					for (const file of files) {
+						let symbolName = file.slice(folder.length+1, -4);
+						symbolName = symbolName.replace(/\\/g, '/');
+						complets.push(new vscode.CompletionItem(symbolName, vscode.CompletionItemKind.File));
+					}
+					return complets;
+
+				}
+				return undefined;
+			}
+			
+		},
+		'"'
+	);
+	context.subscriptions.push(providerUses, providerFiles);
+
 }
 class CtrlDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
 	public provideDocumentSymbols(document: vscode.TextDocument,
@@ -52,6 +90,36 @@ class CtrlDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
 		let symbols = new CtrlSymbolsCreator(document);
 		resolve(symbols.GetSymbols());
 	});}
+}
+
+function ThroughDirectory(directory: string, allFiles: string[]) {
+	let files = fs.readdirSync(directory);
+	files.forEach(file => {
+		const pathFile = path.join(directory, file);
+		if (fs.statSync(pathFile).isDirectory() && file != '.git') {
+			let innerFiles = ThroughDirectory(pathFile, allFiles);
+		}
+		else {
+			allFiles.push(pathFile);
+		}
+	});
+}
+function  GetProjectsInConfigFile(): string[] {
+	let paths = [];
+	let regexp =/proj_path = \"(.*?)\"/g;
+	let workspaceFolders = vscode.workspace.workspaceFolders;
+	if(workspaceFolders)
+	{
+		let fsPath = workspaceFolders[0].uri.fsPath;
+		if (fs.existsSync(fsPath + '/config/config')) {
+			let fileData = fs.readFileSync(fsPath + '/config/config', 'utf8');
+			let result;
+			while (result = regexp.exec(fileData)) {
+				paths.push(result[1])
+			}
+		}
+	}
+	return paths;
 }
 // this method is called when your extension is deactivated
 export function deactivate() {}
