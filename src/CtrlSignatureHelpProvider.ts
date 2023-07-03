4ec9ecc42198bbaa3332d27dc0c921e18380969d
Danil <Akhmatdinov.dr@fixline.tech>
2023-07-03 14:39:00 +0500

    Улучшен комментарий в сигнатуре

--- src/CtrlSignatureHelpProvider.ts
+++ src/CtrlSignatureHelpProvider.ts
@@ -12,7 +12,7 @@ export class CtrlSignatureHelpProvider implements vscode.SignatureHelpProvider {
 			return hoverProvider.GetSignatureHover(document, posVar, token).then((comment) => {
 				if(comment) {
 					const countVarsInFuns = comment[1].slice(comment[1].indexOf('(')+1, comment[1].length-1).split(',');
-					let signatureInformation = new vscode.SignatureInformation(comment[1],comment[0]);
+					let signatureInformation = new vscode.SignatureInformation(comment[1], comment[0].replace(/\t+/g, ' '));
 					signatureInformation.parameters = [new vscode.ParameterInformation(countVarsInFuns[countInputVars.length-1])];
 					signature.signatures = [signatureInformation];
 				}
