aebe71d764fa6aa8ef49c2d3f4a9d45e42741919
Danil <Finist3101@gmail.com>
2024-07-16 22:08:13 +0500

    Доработка форматтера

--- src/CtrlFormatCode.ts
+++ src/CtrlFormatCode.ts
@@ -21,14 +21,15 @@ export class CtrlCodeFormatter implements vscode.DocumentFormattingEditProvider 
             this.lineText = line.text;
             let countCharsInLine = this.lineText.length;
             for (let j = 0; j <= countCharsInLine; j++) {
-                const char = this.lineText.charAt(j);
                 if (j == 0 && !(isLiteralString || isComment)) {
-                    if (this.iterTabs > 0 && line.text.slice(0, line.firstNonWhitespaceCharacterIndex) != '\t'.repeat(this.iterTabs)) {
+                    if (this.iterTabs > 0 && line.text.slice(0, line.firstNonWhitespaceCharacterIndex) != '\t'.repeat(this.iterTabs)
+                        && line.text.charAt(line.firstNonWhitespaceCharacterIndex) != '}') {
                         this.bufferChanges.push(vscode.TextEdit.replace(new vscode.Range(new vscode.Position(i, 0), new vscode.Position(i, line.firstNonWhitespaceCharacterIndex)), '\t'.repeat(this.iterTabs)));
                         this.lineText = '\t'.repeat(this.iterTabs) + this.lineText.slice(line.firstNonWhitespaceCharacterIndex, this.lineText.length);
                         countCharsInLine = this.lineText.length;
                     }
                 }
+                const char = this.lineText.charAt(j);
                 switch (char) {
                     case '"':
                         if (isLiteralString) {
@@ -62,14 +63,23 @@ export class CtrlCodeFormatter implements vscode.DocumentFormattingEditProvider 
                         break;
                     case '{':
                         if (isLiteralString || isComment) break;
-                        // this.formatScope(i, j, this.lineText, '\t'.repeat(this.iterTabs));
+                        this.formatScope(i, j, this.lineText, '\t'.repeat(this.iterTabs));
                         this.iterTabs++;
                         break;
                     case '}':
                         if (isLiteralString || isComment) break;
                         this.iterTabs--;
-                        // this.formatScope(i, j, this.lineText, '\t'.repeat(this.iterTabs));
+                        this.formatScope(i, j, this.lineText, '\t'.repeat(this.iterTabs));
                         break;
+                    case '=':
+                        if (isLiteralString || isComment) break;
+                        this.addSpaces(i, j);
+                        break;
+                    // case '(':
+                    // case ')':
+                    //     if (isLiteralString || isComment) break;
+                    //     this.replaceSpaces(i, j);
+                    //     break;
                     default:
                         break;
                 }
@@ -100,5 +110,49 @@ export class CtrlCodeFormatter implements vscode.DocumentFormattingEditProvider 
             iterChar--;
         }
         this.bufferChanges.push(vscode.TextEdit.replace(new vscode.Range(new vscode.Position(lineNumber, iterChar), new vscode.Position(lineNumber, endDeletePos)), insertString));
+    }
+
+    private addSpaces(lineNumber: number, iterChar: number) {
+        if (this.lineText.length > iterChar) {
+            const nextChar = this.lineText.charAt(iterChar + 1);
+            const idx = nextChar.match(/=|\+|-|\*|\/|\s/);
+            if (!idx) {
+                this.lineText = this.lineText.slice(0, iterChar) + ' ' + this.lineText.slice(iterChar + 1, this.lineText.length);
+                this.bufferChanges.push(vscode.TextEdit.insert(new vscode.Position(lineNumber, iterChar + 1), ' '));
+            }
+        }
+        if (iterChar > 1) {
+            const prevChar = this.lineText.charAt(iterChar - 1);
+            const idx = prevChar.match(/=|\+|-|\*|\/|\s/);
+            if (!idx) {
+                this.lineText = this.lineText.slice(0, iterChar - 1) + ' ' + this.lineText.slice(iterChar, this.lineText.length);
+                this.bufferChanges.push(vscode.TextEdit.insert(new vscode.Position(lineNumber, iterChar), ' '));
+            }
+        }
+    }
+
+    private replaceSpaces(lineNumber: number, iterChar: number) {
+        while (this.lineText.length > iterChar) {
+            const nextChar = this.lineText.charAt(iterChar + 1);
+            if (nextChar == ' ' || nextChar == '\t') {
+                this.lineText = this.lineText.slice(0, iterChar) + this.lineText.slice(iterChar, this.lineText.length);
+                this.bufferChanges.push(vscode.TextEdit.delete(new vscode.Range(new vscode.Position(lineNumber, iterChar), new vscode.Position(lineNumber, iterChar))));
+            }
+            else {
+                break;
+            }
+            iterChar++;
+        }
+        while (iterChar > 1) {
+            const prevChar = this.lineText.charAt(iterChar - 1);
+            if (prevChar == ' ' || prevChar == '\t') {
+                this.lineText = this.lineText.slice(0, iterChar - 1) + this.lineText.slice(iterChar, this.lineText.length);
+                this.bufferChanges.push(vscode.TextEdit.delete(new vscode.Range(new vscode.Position(lineNumber, iterChar - 1), new vscode.Position(lineNumber, iterChar - 1))));
+            }
+            else {
+                break;
+            }
+            iterChar--;
+        }
     }
 }
